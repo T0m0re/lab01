995e1474580253adb673f03739c7f803068a3dca
Tomore <hassanwahab147@gmail.com>
2025-12-23 22:22:19 +0100

    feat: Add exit animation for x-button on NavScreen close event

--- src/components/NavScreen.tsx
+++ src/components/NavScreen.tsx
@@ -91,6 +91,12 @@ useGSAP(()=>{
   useEffect(() => {
 
     if(isClosing){
+
+      gsap.to('.x-button', {
+      opacity: 0,
+      xPercent: 100
+      })
+
       gsap.to('.overlay_group', 
       {
         yPercent: 0,
